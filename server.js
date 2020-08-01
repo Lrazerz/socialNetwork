5c5ad11132b13cadb0077d4e067f56aed5b93293
lrazerz <lrazerz9ine@gmail.com>
2020-08-01 11:00:31 +0300

    prepared to deploy

--- server.js
+++ server.js
@@ -9,15 +9,19 @@ const PORT = process.env.PORT || 5000;
 // connect db
 connectDB();
 
-app.get('/', (req,res) => {
-  res.send('Hello');
-})
-
 // define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log('server running on port', PORT);
