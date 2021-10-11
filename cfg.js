67f068cce918f72ca8c4aee1bf2a803ab2817e27
VladyslavBondarenko1 <vladyslav@reply.io>
2021-10-11 13:43:18 +0300

    add cfg

---
+++ cfg.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const mongoDB = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoDB, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true
+            },
+            err => {
+                if (err) {
+                    throw err;
+                }
+                console.log('database connected');
+            });
+
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
