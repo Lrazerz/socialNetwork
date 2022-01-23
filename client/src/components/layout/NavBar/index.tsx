abe1465365b284165543e81526403473bc8656a3
lrazerz <lrazerz9ine@gmail.com>
2022-01-23 15:38:20 +0200

    change test key, rm redundant spinner

--- client/src/components/layout/NavBar/index.tsx
+++ client/src/components/layout/NavBar/index.tsx
@@ -3,17 +3,13 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { DevConnectorStore } from 'store/types';
-import Spinner from 'components/layout/Spinner';
-
 import AuthLinks from './AuthLinks';
 import GuestLinks from './GuestLinks';
 
 const NavBar: FC = () => {
   const { isAuthenticated, loading } = useSelector(({ auth }: DevConnectorStore) => auth);
 
-  if (loading) {
-    return <Spinner />;
-  }
+  const Links = isAuthenticated ? AuthLinks : GuestLinks;
 
   return (
     <nav className="navbar bg-dark">
@@ -23,7 +19,7 @@ const NavBar: FC = () => {
           DevConnector
         </Link>
       </h1>
-      {isAuthenticated ? <AuthLinks /> : <GuestLinks />}
+      {!loading && <Links />}
     </nav>
   );
 };
